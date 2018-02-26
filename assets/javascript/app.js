c71289c06499005e6c78284eb4f458dd139d6f63
laurenfv <laurenfventura@gmail.com>
2018-02-25 22:59:30 -0500

    added additional functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    var count = 31;
+
     //create array of objects for tivia questions
     var trivia = [
         {"question":"Who painted The Girl With the Pearl Earring","correctAnswer":"Vermeer","wrongAnswer1":"Manet","wrongAnswer2":"Whistler","wrongAnswer3":"Cassat"},
@@ -7,52 +9,70 @@ $(document).ready(function(){
     ];
 
     //create init game function
-    var initializeGame = function(){
-
+    var startGame = function(){
+        var counter = setInterval(function(){
+            count = count - 1;
+            if (count <= 0){
+                clearInterval(counter);
+                //counter ended, do something here
+                return;
+            }
+            $('#time-remaining').text("Time Remaining: " + count + " seconds");
+        }, 1000); //1000 will  run it every 1 second
     }
 
     //create timer function
-    var count=30;
+    // count = 31;
 
-    var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+    // var counter = setInterval(function(){
+    //     count = count - 1;
+    //     if (count <= 0){
+    //         clearInterval(counter);
+    //         //counter ended, do something here
+    //         return;
+    //     }
+    //     $('#time-remaining').text("Time Remaining: " + count + " seconds");
+    // }, 1000); //1000 will  run it every 1 second
 
-    var qTimer = function(){
-        count=count-1;
-        if (count <= 0){
-            clearInterval(counter);
-            //counter ended, do something here
-            return;
-        }
-        //Do code for showing the number of seconds here
-    }
-
-
+    
     //create function to loop through all objects(questions) in array and display them display with answers at random
     var displayQuestion = function(){
         for (i = 0; i < trivia.length; i++){
-            $('#questions').append(trivia[i].question);
+            $('#questions').text(trivia[i].question);
         }
     }
 
     //create function to loop through all objects(answers) in array and display them display as UL at random
     var displayAnswer = function(){
         for (i = 0; i < trivia.length; i++){
-            console.log(trivia[i].correctAnswer);
+            $('#answers').append('<li>' + trivia[i].correctAnswer + '</li>').append('<li>' + trivia[i].wrongAnswer1 + '</li>').append('<li>' + trivia[i].wrongAnswer2 + '</li>').append('<li>' + trivia[i].wrongAnswer3 + '</li>');
         }
     }
 
-    displayAnswer();
-
     var randomizeAnswers = function(){
-        for (i = 0; i < trivia.length; i++){
-            var random = Math.floor(Math.random(trivia[i]);
-        }
+
     }
 
     //create function to handle click
-    var handleClick = function(){
+    var handleFirstClick = function(){
+        $("button").on("click", function() {
+        
+            //When button is pressed, show game play container
+            $("button").hide();
+    
+            // Call startGame so we can set the state of our app
+            startGame();
+    
+        });
+    }
+
+    handleFirstClick();
+
+    //handles on.click event for answers
+    var handleAnswerClick = function(){
 
     }
+
 
     //create conditional to check for right answer / answer will always be array[i].correctAnswer
     var checkAnswer = function(){
